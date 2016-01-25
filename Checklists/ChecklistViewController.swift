c92f927a3c285dc858d2328f1cb1c6364e8c2008
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-25 08:36:50 +0200

    Making 20 times the same 5 rows

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -21,7 +21,7 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(tableView: UITableView,
@@ -29,15 +29,15 @@ class ChecklistViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier( "ChecklistItem", forIndexPath: indexPath)
             let label = cell.viewWithTag(1000) as! UILabel
             //Here you ask the table view cell for the view with tag 1000. That is the tag you set on the label in the storyboard, so this returns a reference to the corresponding UILabel object.
-            if indexPath.row == 0 {
+            if indexPath.row % 5 == 0 {
                 label.text = "Walk the dog"
-            } else if indexPath.row == 1 {
+            } else if indexPath.row % 5 == 1 {
                 label.text = "Brush my teeth"
-            } else if indexPath.row == 2 {
+            } else if indexPath.row % 5 == 2 {
                 label.text = "Learn iOS development"
-            } else if indexPath.row == 3 {
+            } else if indexPath.row % 5 == 3 {
                 label.text = "Soccer practice"
-            } else if indexPath.row == 4 {
+            } else if indexPath.row % 5 == 4 {
                 label.text = "Eat ice cream"
             }
         return cell
