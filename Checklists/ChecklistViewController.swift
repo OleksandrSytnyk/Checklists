8cfb9f6b2d0c58d56f922605983f90bdcef490c1
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-25 08:33:28 +0200

    Putting row data into the cells

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -21,12 +21,25 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier( "ChecklistItem", forIndexPath: indexPath)
+            let label = cell.viewWithTag(1000) as! UILabel
+            //Here you ask the table view cell for the view with tag 1000. That is the tag you set on the label in the storyboard, so this returns a reference to the corresponding UILabel object.
+            if indexPath.row == 0 {
+                label.text = "Walk the dog"
+            } else if indexPath.row == 1 {
+                label.text = "Brush my teeth"
+            } else if indexPath.row == 2 {
+                label.text = "Learn iOS development"
+            } else if indexPath.row == 3 {
+                label.text = "Soccer practice"
+            } else if indexPath.row == 4 {
+                label.text = "Eat ice cream"
+            }
         return cell
     }
     
