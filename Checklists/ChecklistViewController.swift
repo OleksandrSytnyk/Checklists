057c0f8d4b2adfd9b91b1f94c8567b899dd94f8a
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-25 12:16:59 +0200

    Implementing the Model-View-Controller conception
    Beginning

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+    var row0text = "Walk the dog"
+    var row1text = "Brush teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Soccer practice"
+    var row4text = "Eat ice cream"
+//These five instance variables are the data model.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,7 +27,7 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(tableView: UITableView,
@@ -29,17 +35,17 @@ class ChecklistViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier( "ChecklistItem", forIndexPath: indexPath)
             let label = cell.viewWithTag(1000) as! UILabel
             //Here you ask the table view cell for the view with tag 1000. That is the tag you set on the label in the storyboard, so this returns a reference to the corresponding UILabel object.
-            if indexPath.row % 5 == 0 {
+            if indexPath.row == 0 {
                 //This uses the remainder operator, represented by the % sign
-                label.text = "Walk the dog"
-            } else if indexPath.row % 5 == 1 {
-                label.text = "Brush my teeth"
-            } else if indexPath.row % 5 == 2 {
-                label.text = "Learn iOS development"
-            } else if indexPath.row % 5 == 3 {
-                label.text = "Soccer practice"
-            } else if indexPath.row % 5 == 4 {
-                label.text = "Eat ice cream"
+                label.text = row0text
+            } else if indexPath.row == 1 {
+                label.text = row1text
+            } else if indexPath.row == 2 {
+                label.text = row2text
+            } else if indexPath.row == 3 {
+                label.text = row3text
+            } else if indexPath.row == 4 {
+                label.text = row4text
             }
         return cell
     }
