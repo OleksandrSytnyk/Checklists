354f7c3b5d24c99d4ca39b459e123151bd20e95b
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-27 18:16:01 +0200

    static sell with Text field

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -2,7 +2,7 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
-
+    
     @IBAction func cancel() {
     dismissViewControllerAnimated(true, completion: nil)
     }
@@ -10,5 +10,10 @@ class AddItemViewController: UITableViewController {
     @IBAction func done() {
     dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    //When the user taps in a cell, the table view sends the delegate a willSelectRowAtIndexPath message that says: “Hi delegate, I am about to select this particular row.” By returning the special value nil, the delegate answers: “Sorry, but you're not allowed to!”
 
 }
