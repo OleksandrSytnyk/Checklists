231f8a4cd3ce5a1b25a7febec3956c537c89b6ed
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-27 15:17:14 +0200

    Making feature go back from add to main controller

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    @IBAction func cancel() {
+    dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func done() {
+    dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
