499c326a479eb99061ad9b294a6d9e182275f17a
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-28 06:22:38 +0200

    polyshing code up
    textField.becomeFirstResponder plus the Did End on Exit event for textField and preventing empty text when toggling done button on the keyboard (Auto-enable Return Key)

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -18,5 +18,11 @@ class AddItemViewController: UITableViewController {
         return nil
     }
     //When the user taps in a cell, the table view sends the delegate a willSelectRowAtIndexPath message that says: “Hi delegate, I am about to select this particular row.” By returning the special value nil, the delegate answers: “Sorry, but you're not allowed to!”
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+        //giving the control focus
+    }
 
 }
