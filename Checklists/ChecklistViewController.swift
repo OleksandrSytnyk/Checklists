ba83ce3d6bbb0472cbd6bb689661e6a30e93dcdc
OleksandrSytnyk <amsitnik@gmail.com>
2016-02-03 15:36:00 +0200

    Replacing items from ChecklistViewController
    The first stage - deleting from old place

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,17 +9,7 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController, ItemDetailViewControllerDelegate {
-    var items: [ChecklistItem]
-    var checklist: Checklist!
-    
-    required init?(coder aDecoder: NSCoder) {
-        items = [ChecklistItem]()
-        super.init(coder: aDecoder)
-        loadChecklistItems()
-        print("Documents folder is \(documentsDirectory())")
-        print("Data file path is \(dataFilePath())")
-        //this is to check whether two new methods work correctly. Just here because all variables are initiated and this method will executed wih guarantee
-    }
+        var checklist: Checklist!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,13 +22,13 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return checklist.items.count
     }
     
     override func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier( "ChecklistItem", forIndexPath: indexPath)
-            let item = items[indexPath.row]
+            let item = checklist.items[indexPath.row]
             configureTextForCell(cell, withChecklistItem: item) 
             configureCheckmarkForCell(cell, withChecklistItem: item)
         return cell
@@ -49,19 +39,17 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
                     //The checkmark is part of the cell (the accessory, remember?), so you first need to find the UITableViewCell object for the tapped row.
                     //this is not the same method as the data source method tableView(cellForRowAtIndexPath).
                     //Because it is theoretically possible that there is no cell at the specified index-path,for example if that row isn’t visible, you need to use the special if let statement.
-                let item = items[indexPath.row]
+                let item = checklist.items[indexPath.row]
         item.toggleChecked()
         configureCheckmarkForCell(cell, withChecklistItem: item)
             }
                 tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        saveChecklistItem()
     }
     
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        items.removeAtIndex(indexPath.row)
+        checklist.items.removeAtIndex(indexPath.row)
         let indexPaths = [indexPath]
         tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
-        saveChecklistItem()
     }
     //When the commitEditingStyle method is present in your view controller (it comes from the table view data source), the table view will automatically enable swipe-to- delete. This is the swipe-to-delete function.
     
@@ -84,24 +72,22 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     }
     
     func itemDetailViewController(controller: ItemDetailViewController, didFinishAddingItem item: ChecklistItem) {
-        let newRowIndex = items.count
-        items.append(item)
+        let newRowIndex = checklist.items.count
+        checklist.items.append(item)
         let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
         let indexPaths = [indexPath]
         tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
         
         dismissViewControllerAnimated(true, completion: nil)
-        saveChecklistItem()
     }
     
     func itemDetailViewController( controller:  ItemDetailViewController, didFinishEditingItem item: ChecklistItem) {
-        if let index = items.indexOf(item) {
+        if let index = checklist.items.indexOf(item) {
             let indexPath = NSIndexPath(forRow: index, inSection: 0)
             if let cell = tableView.cellForRowAtIndexPath(indexPath) {
                 configureTextForCell(cell, withChecklistItem: item) }
         }
         dismissViewControllerAnimated(true, completion: nil)
-        saveChecklistItem()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -118,39 +104,9 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         controller.delegate = self
         if let indexPath = tableView.indexPathForCell(
             sender as! UITableViewCell) {
-            controller.itemToEdit = items[indexPath.row]
+            controller.itemToEdit = checklist.items[indexPath.row]
             }
         }
     }
-    
-    func documentsDirectory() -> String {
-        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-    return paths[0]
-    }
-    
-    func dataFilePath() -> String {
-        return (documentsDirectory() as NSString).stringByAppendingPathComponent("Checklists.plist")
-    }
-    
-    func saveChecklistItem() {
-        let data = NSMutableData()
-        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
-        //NSKeyedArchiver is a form of NSCoder that creates plist files
-        archiver.encodeObject(items, forKey: "ChecklistItems")
-        archiver.finishEncoding()
-        data.writeToFile(dataFilePath(), atomically: true)
-    }
-    
-    func loadChecklistItems() {
-        let path = dataFilePath()
-        if NSFileManager.defaultManager().fileExistsAtPath(path) {
-        if let data = NSData(contentsOfFile: path) {
-        let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
-        items = unarchiver.decodeObjectForKey("ChecklistItems")
-        as! [ChecklistItem]
-        unarchiver.finishDecoding() }
-        }
-    }
-    
 }
 
