1f369423740dd2fbee89331b6d6bce5fe2c572f0
OleksandrSytnyk <amsitnik@gmail.com>
2016-02-06 16:19:06 +0200

    Adding a screen to pick up an icon for a Checklist

--- Checklists/ListDetailViewController.swift
+++ Checklists/ListDetailViewController.swift
@@ -11,6 +11,7 @@ protocol ListDetailViewControllerDelegate: class {
     class ListDetailViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     @IBOutlet weak var textField: UITextField!
+        @IBOutlet weak var iconImageView: UIImageView!
         weak var delegate: ListDetailViewControllerDelegate?
         var checklistToEdit: Checklist?
         
@@ -44,14 +45,12 @@ protocol ListDetailViewControllerDelegate: class {
         }
         
         override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+            if indexPath.section == 1 {
+                return indexPath
+            } else {
             return nil
+            }
         }
-        
-        /*func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange,
-        replacementString string: String) -> Bool { let oldText: NSString = textField.text!
-        let newText: NSString = oldText.stringByReplacingCharactersInRange( range, withString: string)
-        doneBarButton.enabled = (newText.length > 0)
-        return true }*/
         
         func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
             let oldText: NSString = textField.text!
